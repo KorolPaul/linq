85b3b40e2936f7ee9a556b575eadeea5e65fff7d
Pavel Korol <korol.paul@gmail.com>
2023-05-26 21:24:31 +0300

    speakers and faq page

--- src/js/main.js
+++ src/js/main.js
@@ -542,43 +542,47 @@ if (Cookies) {
 const accordionElements = document.querySelectorAll('.accordion');
 
 accordionElements.forEach(accordion => {
-    const accordionButtons = accordion.querySelectorAll('.accordion_button:not(.accordion_button__inner)');
-    const accordionContent = accordion.querySelectorAll('.accordion_content:not(.accordion_content__inner)');
-    const accordionInnerButtons = accordion.querySelectorAll('.accordion_button__inner');
-
-
-    if (accordionButtons.length) {
-        function toggle(e) {
-            e.preventDefault();
-
-            if (e.target.classList.contains('active')) {
-                e.target.classList.remove('active');
-                e.currentTarget.nextElementSibling.classList.remove('active');
-                e.currentTarget.parentElement.classList.remove('active');
-                return;
-            }
-
-            if (isDesktop) {
-                e.currentTarget.classList.add('active');
-                e.currentTarget.nextElementSibling.classList.add('active');
-                e.currentTarget.parentElement.classList.add('active');
-            } else {
-                e.currentTarget.classList.toggle('active');
-                e.currentTarget.nextElementSibling.classList.toggle('active');
-                e.currentTarget.parentElement.classList.toggle('active');
-            }
+    const accordionButtons = accordion.querySelectorAll('.accordion_button');
+    
+    function toggle(e) {
+        e.preventDefault();
+        
+        if (e.target.classList.contains('active')) {
+            e.target.classList.remove('active');
+            e.currentTarget.nextElementSibling.classList.remove('active');
+            e.currentTarget.parentElement.classList.remove('active');
+            return;
         }
-
-        function toggleInner(e) {
-            e.preventDefault();
-
+        
+        if (isDesktop) {
+            e.currentTarget.classList.add('active');
+            e.currentTarget.nextElementSibling.classList.add('active');
+            e.currentTarget.parentElement.classList.add('active');
+        } else {
             e.currentTarget.classList.toggle('active');
             e.currentTarget.nextElementSibling.classList.toggle('active');
+            e.currentTarget.parentElement.classList.toggle('active');
+        }
+    }
+    
+    
+    accordionButtons.forEach(el => el.addEventListener('click', toggle));
+    
+    const accordionTabButtons = accordion.querySelectorAll('.accordion_tab-button');
+    const accordionTabContent = accordion.querySelectorAll('.accordion_tab-content');
+
+    accordionTabButtons.forEach(el => el.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        if (e.target.classList.contains('active')) {
+            return;
         }
 
-        accordionButtons.forEach(el => el.addEventListener('click', toggle));
-        accordionInnerButtons.forEach(el => el.addEventListener('click', toggleInner));
-    }
+        accordionTabButtons.forEach(el => el.classList.remove('active'));
+        accordionTabContent.forEach(el => el.classList.remove('active'));
+        e.target.classList.add('active');
+        e.target.nextElementSibling.classList.add('active');
+    }));
 });
 
 /* popup form */
